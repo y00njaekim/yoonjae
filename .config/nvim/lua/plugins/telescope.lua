a8bdb3c70452b9ddec914101b51d53b351b48c9d
y00njaekim <rladbswo12@gm.gist.ac.kr>
2026-07-16 18:45:57 +0900

    add telescope plugin

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -6,8 +6,18 @@ return {
         'nvim-lua/plenary.nvim',
         -- optional but recommended
         { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+        'nvim-telescope/telescope-ui-select.nvim',
     },
     config = function()
+        local telescope = require("telescope")
+
+        telescope.setup({
+            extensions = {
+                ["ui-select"] = require("telescope.themes").get_dropdown({}),
+            },
+        })
+        telescope.load_extension("ui-select")
+
         local builtin = require("telescope.builtin")
         mapKey('<leader>ff', builtin.find_files)
         mapKey('<leader>fg', builtin.live_grep)
