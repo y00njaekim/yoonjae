01707a36724b9794813c732719522e6e5e9312d7
y00njaekim <rladbswo12@gm.gist.ac.kr>
2026-07-28 16:56:23 +0900

    add vim-style neo-tree navigation

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -8,5 +8,13 @@ return {
       "nvim-tree/nvim-web-devicons", -- optional, but recommended
     },
     lazy = false, -- neo-tree will lazily load itself
-  }
+    opts = {
+      window = {
+        mappings = {
+          ["l"] = "open",
+          ["h"] = "close_node",
+        },
+      },
+    },
+  },
 }
