de9fe1cbe0139aeec670ea846095ca87919732d1
y00njaekim <yoonjae.kim@makinarocks.ai>
2026-08-09 17:25:46 +0900

    Add file location copy keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -18,6 +18,33 @@ mapKey('<C-l>', '<C-w>l') -- Right
 -- clear search highlights 
 mapKey('<leader>h', ':nohlsearch<CR>')
 
+-- copy relative file path with line number
+mapKey('<leader>yl', function()
+	local filepath = vim.fn.expand('%:.')
+	if filepath == '' then
+		vim.notify('Current buffer has no file path', vim.log.levels.WARN)
+		return
+	end
+
+	local location = string.format('%s:%d', filepath, vim.fn.line('.'))
+	vim.fn.setreg('+', location)
+	vim.notify('Copied: ' .. location)
+end)
+
+mapKey('<leader>yl', function()
+	local filepath = vim.fn.expand('%:.')
+	if filepath == '' then
+		vim.notify('Current buffer has no file path', vim.log.levels.WARN)
+		return
+	end
+
+	local start_line = vim.fn.line("'<")
+	local end_line = vim.fn.line("'>")
+	local location = string.format('%s:%d-%d', filepath, start_line, end_line)
+	vim.fn.setreg('+', location)
+	vim.notify('Copied: ' .. location)
+end, 'v')
+
 -- indent 
 mapKey('<', '<gv', 'v')
 mapKey('>', '>gv', 'v')
