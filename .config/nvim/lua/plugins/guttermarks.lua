8264927a859d176171320c64e4757287e3477669
y00njaekim <yoonjae.kim@makinarocks.ai>
2026-09-21 10:41:40 +0900

    modify guttermarks plugin

--- .config/nvim/lua/plugins/guttermarks.lua
+++ .config/nvim/lua/plugins/guttermarks.lua
@@ -3,6 +3,9 @@ return {
     "dimtion/guttermarks.nvim",
     event = { "BufReadPost", "BufNewFile", "BufWritePre", "FileType" },
     opts = {
+      local_mark = { priority = 100 },
+      global_mark = { priority = 100 },
+      special_mark = { priority = 100 },
       excluded_filetypes = { "NvimTree", "neo-tree", "alpha" },
     },
   },
